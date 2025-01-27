f2fcbfd7d192c80d2f637a8bdff9cb8c5e978d02
Chioma Obiajulu <chibabebest01@gmail.com>
2025-01-27 12:06:35 +0100

    first commit

---
+++ devbloggers/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for devbloggers project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'devbloggers.settings')
+
+application = get_asgi_application()
