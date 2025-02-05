e7f56da58a7f5b3683f04057a7100f6c2c90bb79
Chioma Obiajulu <chibabebest01@gmail.com>
2025-02-05 02:28:09 +0100

    did some more work on the profile page and added some changes to the profile model

--- users/views.py
+++ users/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from .models import Profile
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url='account_login')
 def profile(request, pk):
     return render(request, 'users/profile.html')
 
+@login_required(login_url='account_login')
 def edit_profile(request, pk):
     return render(request, 'users/edit-profile.html')
